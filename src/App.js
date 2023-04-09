b68619be6b4f39ea32f6b6fb1ecf98432c695233
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-04-09 17:54:07 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header/header';
 
 function App() {
-
   return (
     <div className="App">
-      <h1>Testjhbhjnlk</h1>
       <Header />
     </div>
   );

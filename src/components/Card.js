1f927c645abfd23db03e836ba50023cf7e5a4a58
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-04-29 16:19:58 +0200

    React props

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Card = ({title, description, image}) => {
     return(
        <div>
-        <img src={image} alt={title} />
+        <img src={image} />
         <div>
             <h1>{title}</h1>
-            <h1>{description}</h1>
+            <p>{description}</p>
         </div>
        </div>
     )

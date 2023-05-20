12deb78900ee4a317775687d770e833b47719309
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-05-20 20:45:03 +0200

    React Routing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css';
-import Login from './state/login';
-
-const cardData = [
-  {
-    id: 1,
-    image: 'https://example.com/image1.jpg',
-    title: 'Card 1',
-    description: 'This is the first card.',
-  },
-  {
-    id: 2,
-    image: 'https://example.com/image2.jpg',
-    title: 'Card 2',
-    description: 'This is the second card.',
-  },
-  {
-    id: 3,
-    image: 'https://example.com/image3.jpg',
-    title: 'Card 3',
-    description: 'This is the third card.',
-  },
-];
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import Header from './pages/header';
 
 function App() {
   return (
-    <div>
-   <Login />
-    </div>
-  
-    // <div className="App">
-    //  {cardData.map((card) => (
-    //   <Card
-    //     key={card.id}
-    //     image={card.image}
-    //     title={card.title}
-    //     description={card.description}
-    //   />
-    //  ))}
-    // </div>
-    
+   <>
+   <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+   </>
   );
 }
 

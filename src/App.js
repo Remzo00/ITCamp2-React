f993ce524ffe6bd96f95f9f0c60f7ac5e9263a7f
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-05-27 21:00:51 +0200

    Prikaz API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import { Link, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 import Header from './pages/header';
+import axios from 'axios';
 
+// 'https://jsonplaceholder.typicode.com/posts/1'
 function App() {
+  const [data, setData] = useState([])
+
+const fetchApi = async () => {
+  try{
+    const response = await axios.get('https://fakestoreapi.com/products')
+    setData(response.data)
+  }catch(error){
+    console.log("Error: ", error)
+  }
+}
+
+useEffect(() => {
+  fetchApi()
+}, [])
+
+
   return (
    <>
-   <Header />
+   {data.map((product) => (
+    <div>
+      <div key={product.id}>
+        <img src={product.image} />
+      </div>
+      <div>
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+        <p>{product.price}</p>
+      </div>
+    </div>
+   ))}
+   {/* <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/about' element={<About />} />
-    </Routes>
+    </Routes> */}
    </>
   );
 }

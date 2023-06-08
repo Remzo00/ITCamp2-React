cd905aa1db677172c6403aaf061fdf4ca671d16b
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-06-08 20:32:05 +0200

    Material UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
-import { Link, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
-import About from './pages/about';
-import Header from './pages/header';
-import axios from 'axios';
 import Parent from './props/parent';
+import { Button } from '@mui/material';
 
 // 'https://jsonplaceholder.typicode.com/posts/1'
 function App() {
   return (
    <>
-    <Parent />
+    <Button variant="contained">Hello</Button>
    </>
   );
 }

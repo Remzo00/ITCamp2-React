739e7f63c5d62ea9bbc021c04fd9d43f258aabcd
Remzo Gusinac <gusinacremzo00@gmail.com>
2023-06-17 13:22:19 +0200

    react query

---
+++ src/query/index.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
+
+const fetchData = async () => {
+    const response = await axios.get('https://fakestoreapi.com/products')
+    const data = response.data
+    return data
+}
+
+const ReactQuery = () => {
+    const { data, isLoading, isError, error } = useQuery('products', fetchData)
+
+    if(isLoading){
+        return <div>Loading...</div>
+    }
+
+    if(isError){
+        return <div>Error: {error.message}</div>
+    }
+    return(
+        <div>
+      <h1>Products List</h1>
+      <ul>
+        {data.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+    )
+}
+
+export default ReactQuery
